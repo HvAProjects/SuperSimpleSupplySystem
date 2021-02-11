9835830eba2b23c41c7f1beff06ea14ccb2d226b
David Soff <david@soff.nl>
2021-02-11 21:26:37 +0100

    Initial commit setting up spring

---
+++ backend/src/main/java/nl/jed/supersimplesupplysystem/SupersimplesupplysystemApplication.java
@@ -0,0 +1,13 @@
+package nl.jed.supersimplesupplysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupersimplesupplysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupersimplesupplysystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nl/jed/supersimplesupplysystem/SupersimplesupplysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jed.supersimplesupplysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupersimplesupplysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
