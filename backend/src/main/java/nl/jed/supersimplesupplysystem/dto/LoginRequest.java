1824e48fb35d65561682235264dfc9e75a57abd7
evaanp <47040647+evaanp@users.noreply.github.com>
2021-02-19 12:57:54 +0100

    Update backend/src/main/java/nl/jed/supersimplesupplysystem/dto/LoginRequest.java
    Co-authored-by: David Soff <david@soff.nl>

--- backend/src/main/java/nl/jed/supersimplesupplysystem/dto/LoginRequest.java
+++ backend/src/main/java/nl/jed/supersimplesupplysystem/dto/LoginRequest.java
@@ -7,24 +7,10 @@ import lombok.Data;
 @Data
 public class LoginRequest {
     @NotBlank
+    @Getter @Setter
     private String email;
 
     @NotBlank
+    @Getter @Setter
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
