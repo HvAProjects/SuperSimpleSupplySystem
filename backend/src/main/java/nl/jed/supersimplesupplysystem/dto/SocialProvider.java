d5d92e349f4529c901071a211ea29aff58245870
evaanp <47040647+evaanp@users.noreply.github.com>
2021-02-19 12:58:22 +0100

    Update backend/src/main/java/nl/jed/supersimplesupplysystem/dto/SocialProvider.java
    Co-authored-by: David Soff <david@soff.nl>

--- backend/src/main/java/nl/jed/supersimplesupplysystem/dto/SocialProvider.java
+++ backend/src/main/java/nl/jed/supersimplesupplysystem/dto/SocialProvider.java
@@ -4,11 +4,8 @@ public enum SocialProvider {
 
     FACEBOOK("facebook"), TWITTER("twitter"), LINKEDIN("linkedin"), GOOGLE("google"), GITHUB("github"), LOCAL("local");
 
+    @Getter
     private String providerType;
-
-    public String getProviderType() {
-        return providerType;
-    }
 
     SocialProvider(final String providerType) {
         this.providerType = providerType;
