fe9db0c2e3e4867d74e8dc498574ee144401842b
Joe Vrolijk <joe.vrolijk>
2021-03-04 21:30:35 +0100

    #4 - Login Screen Ui now designed and works.

--- frontend/supersimplesupplysystem/src/app/app.module.ts
+++ frontend/supersimplesupplysystem/src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { RegisterComponent } from './pages/register/register.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './pages/login/login.component';
@@ -21,7 +21,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
 import { HouseholdComponent } from './pages/household/household.component';
+
 
 @NgModule({
   declarations: [
@@ -47,7 +52,12 @@ import { HouseholdComponent } from './pages/household/household.component';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDividerModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
