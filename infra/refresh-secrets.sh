cf92d51b0ac685346eae5649e14c8f463753f3c4
David Soff <david@soff.nl>
2021-04-10 22:17:28 +0200

    add convenience script for refreshing secrets

---
+++ infra/refresh-secrets.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# navigate to directory of the script
+cd "$(dirname "$(readlink -f "$0")")"
+
+kubectl create secret generic ssss-secrets-properties \
+--from-file=../backend/src/main/resources/secrets.properties \
+--dry-run=client \
+-o yaml > secrets.yaml
+
+kubeseal --fetch-cert \
+--controller-name=sealed-secrets \
+--controller-namespace=flux-system \
+> pub-sealed-secrets.pem
+
+kubeseal --format=yaml --cert=pub-sealed-secrets.pem \
+< secrets.yaml > ssss/secrets.yaml
+
+rm -Rf secrets.yaml
+rm -Rf pub-sealed-secrets.pem
+
+cd ..
+git reset HEAD -- .
+git add "infra/ssss/secrets.yaml"
+git commit -m "update secrets.yaml"
