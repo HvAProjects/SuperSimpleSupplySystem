032d3228989958ff13a6ee89e022ccd94ba785a4
David Soff <david@soff.nl>
2021-05-19 16:03:09 +0200

    Update setup.sh

--- infra/setup.sh
+++ infra/setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-flux bootstrap github \
-  --components-extra=image-reflector-controller,image-automation-controller \
-  --owner=HvAProjects \
-  --repository=SuperSimpleSupplySystem \
-  --branch=k8s \
-  --path=./infra \
-  --read-write-key=true
+kubectl apply -f flux-system/gotk-components.yaml
+kubectl apply -f infra/flux-system/gotk-sync.yaml
